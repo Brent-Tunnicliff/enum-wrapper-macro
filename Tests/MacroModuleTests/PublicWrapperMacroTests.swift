cfc3aa5dc20845f7d8950288c200ba31450b7be5
Brent Tunnicliff <brent@tunnicliff.dev>
2026-09-05 01:12:26 +1000

    Added more success unit test scenarios.

--- Tests/MacroModuleTests/PublicWrapperMacroTests.swift
+++ Tests/MacroModuleTests/PublicWrapperMacroTests.swift
@@ -365,68 +365,470 @@ extension PublicWrapperMacroTests.SuccessArguments {
     }
 }
 
-extension PublicWrapperMacroTests {
-    struct SupportedRawValueTypeArgument: CaseIterable, CustomStringConvertible, Sendable {
-        static var allCases: [PublicWrapperMacroTests.SupportedRawValueTypeArgument] {
-            #if canImport(MacroModule)
-                RawValueSupportedInheritanceType.allCases.map {
-                    SupportedRawValueTypeArgument.init(description: $0.rawValue)
+#if canImport(MacroModule)
+
+    // MARK: SupportedRawValueTypeArgument
+
+    extension PublicWrapperMacroTests {
+        @Test(arguments: RawValueSupportedInheritanceType.allCases)
+        func supported(rawValueType: RawValueSupportedInheritanceType) {
+            let rawValue = rawValueType.rawValue
+            let input = """
+                @PublicWrapper
+                enum Value: \(rawValue) {
+                    case one
                 }
-            #else
-                []
-            #endif
+                """
+
+            // We expect wrapping RawRepresentable enum types to auto conform the struct to Equatable and Hashable too.
+            let expectedResult = """
+                enum Value: \(rawValue) {
+                    case one
+                }
+
+                public struct ValueWrapper: RawRepresentable, Equatable, Hashable, Sendable {
+                    typealias WrappedValue = Value
+
+                    let wrappedValue: WrappedValue
+
+                    private init(wrappedValue: WrappedValue) {
+                        self.wrappedValue = wrappedValue
+                    }
+
+                    public static let one = Self.init(wrappedValue: .one)
+
+                    // MARK: RawRepresentable
+
+                    public typealias RawValue = \(rawValue)
+
+                    public var rawValue: RawValue {
+                        wrappedValue.rawValue
+                    }
+
+                    public init?(rawValue: RawValue) {
+                        guard let wrappedValue = WrappedValue(rawValue: rawValue) else {
+                            return nil
+                        }
+                        self.wrappedValue = wrappedValue
+                    }
+                }
+                """
+            assertMacroExpansion(
+                input,
+                expandedSource: expectedResult,
+                macroSpecs: testMacros,
+                failureHandler: Issue.record(failure:)
+            )
+        }
+    }
+
+    // MARK: ProtocolSupportedInheritanceType
+
+    extension PublicWrapperMacroTests {
+        static let identifiableArguments: [ProtocolSupportedInheritanceType] = {
+            let rawValueTypes: [SupportedIdentifiableType] = RawValueSupportedInheritanceType.allCases
+                .map { SupportedIdentifiableType.rawValue($0) }
+            let supportedTypes = rawValueTypes + [.uuid, .custom(name: "CustomIdentifiable")]
+            return supportedTypes.map { .identifiable($0) }
+        }()
+
+        static let supportedProtocolTypeArguments: [ProtocolSupportedInheritanceType] = {
+            ProtocolSupportedInheritanceType.TypeName
+                .allCases
+                .flatMap { typeName -> [ProtocolSupportedInheritanceType] in
+                    switch typeName {
+                    case .caseIterable: [.caseIterable]
+                    case .codable: [.codable]
+                    case .comparable: [.comparable]
+                    case .customDebugStringConvertible: [.customDebugStringConvertible]
+                    case .customStringConvertible: [.customStringConvertible]
+                    case .decodable: [.decodable]
+                    case .encodable: [.encodable]
+                    case .equatable: [.equatable]
+                    case .hashable: [.hashable]
+                    case .identifiable: identifiableArguments
+                    case .sendable: [.sendable]
+                    }
+                }
+        }()
+
+        @Test(arguments: supportedProtocolTypeArguments)
+        func supported(protocolType: ProtocolSupportedInheritanceType) {
+            let (input, expectedResult) = values(for: protocolType)
+            assertMacroExpansion(
+                input,
+                expandedSource: expectedResult,
+                macroSpecs: testMacros,
+                failureHandler: Issue.record(failure:)
+            )
         }
 
-        let description: String
-    }
+        private func values(
+            for protocolType: ProtocolSupportedInheritanceType
+        ) -> (input: String, expectedResult: String) {
+            let input: String
+            let expectedResult: String
 
-    @Test(arguments: SupportedRawValueTypeArgument.allCases)
-    func supported(rawValueType: SupportedRawValueTypeArgument) {
-        let input = """
-            @PublicWrapper
-            enum Value: \(rawValueType) {
-                case one
-            }
-            """
-
-        // We expect wrapping RawRepresentable enum types to auto conform the struct to Equatable and Hashable too.
-        let expectedResult = """
-            enum Value: \(rawValueType) {
-                case one
-            }
-
-            public struct ValueWrapper: RawRepresentable, Equatable, Hashable, Sendable {
-                typealias WrappedValue = Value
-
-                let wrappedValue: WrappedValue
-
-                private init(wrappedValue: WrappedValue) {
-                    self.wrappedValue = wrappedValue
-                }
-
-                public static let one = Self.init(wrappedValue: .one)
-
-                // MARK: RawRepresentable
-
-                public typealias RawValue = \(rawValueType)
-
-                public var rawValue: RawValue {
-                    wrappedValue.rawValue
-                }
-
-                public init?(rawValue: RawValue) {
-                    guard let wrappedValue = WrappedValue(rawValue: rawValue) else {
-                        return nil
+            switch protocolType {
+            case .caseIterable:
+                input = """
+                    @PublicWrapper
+                    enum Value: CaseIterable {
+                        case one
                     }
-                    self.wrappedValue = wrappedValue
-                }
+                    """
+                expectedResult = """
+                    enum Value: CaseIterable {
+                        case one
+                    }
+
+                    public struct ValueWrapper: CaseIterable, Equatable, Hashable, Sendable {
+                        typealias WrappedValue = Value
+
+                        let wrappedValue: WrappedValue
+
+                        private init(wrappedValue: WrappedValue) {
+                            self.wrappedValue = wrappedValue
+                        }
+
+                        public static let one = Self.init(wrappedValue: .one)
+
+                        // MARK: CaseIterable
+
+                        public static let allCases = WrappedValue.allCases.map(Self.init)
+                    }
+                    """
+            case .codable:
+                input = """
+                    @PublicWrapper
+                    enum Value: Codable {
+                        case one
+                    }
+                    """
+                expectedResult = """
+                    enum Value: Codable {
+                        case one
+                    }
+
+                    public struct ValueWrapper: Codable, Equatable, Hashable, Sendable {
+                        typealias WrappedValue = Value
+
+                        let wrappedValue: WrappedValue
+
+                        private init(wrappedValue: WrappedValue) {
+                            self.wrappedValue = wrappedValue
+                        }
+
+                        public static let one = Self.init(wrappedValue: .one)
+
+                        // MARK: Decodable
+
+                        public init(from decoder: any Decoder) throws {
+                            self.wrappedValue = try WrappedValue(from: decoder)
+                        }
+
+                        // MARK: Encodable
+
+                        public func encode(to encoder: any Encoder) throws {
+                            try wrappedValue.encode(to: encoder)
+                        }
+                    }
+                    """
+            case .comparable:
+                input = """
+                    @PublicWrapper
+                    enum Value: Comparable {
+                        case one
+                        case two
+                    }
+                    """
+                expectedResult = """
+                    enum Value: Comparable {
+                        case one
+                        case two
+                    }
+
+                    public struct ValueWrapper: Comparable, Equatable, Hashable, Sendable {
+                        typealias WrappedValue = Value
+
+                        let wrappedValue: WrappedValue
+
+                        private init(wrappedValue: WrappedValue) {
+                            self.wrappedValue = wrappedValue
+                        }
+
+                        public static let one = Self.init(wrappedValue: .one)
+                        public static let two = Self.init(wrappedValue: .two)
+
+                        // MARK: Comparable
+
+                        public static func < (lhs: Self, rhs: Self) -> Bool {
+                            lhs.wrappedValue < rhs.wrappedValue
+                        }
+
+                        public static func <= (lhs: Self, rhs: Self) -> Bool {
+                            lhs.wrappedValue <= rhs.wrappedValue
+                        }
+
+                        public static func >= (lhs: Self, rhs: Self) -> Bool {
+                            lhs.wrappedValue >= rhs.wrappedValue
+                        }
+
+                        public static func > (lhs: Self, rhs: Self) -> Bool {
+                            lhs.wrappedValue > rhs.wrappedValue
+                        }
+                    }
+                    """
+            case .customDebugStringConvertible:
+                input = """
+                    @PublicWrapper
+                    enum Value: CustomDebugStringConvertible {
+                        case one
+
+                        var debugDescription: String {
+                            "hi :)"
+                        }
+                    }
+                    """
+                expectedResult = """
+                    enum Value: CustomDebugStringConvertible {
+                        case one
+
+                        var debugDescription: String {
+                            "hi :)"
+                        }
+                    }
+
+                    public struct ValueWrapper: CustomDebugStringConvertible, Equatable, Hashable, Sendable {
+                        typealias WrappedValue = Value
+
+                        let wrappedValue: WrappedValue
+
+                        private init(wrappedValue: WrappedValue) {
+                            self.wrappedValue = wrappedValue
+                        }
+
+                        public static let one = Self.init(wrappedValue: .one)
+
+                        // MARK: CustomDebugStringConvertible
+
+                        public var debugDescription: String {
+                            wrappedValue.debugDescription
+                        }
+                    }
+                    """
+            case .customStringConvertible:
+                input = """
+                    @PublicWrapper
+                    enum Value: CustomStringConvertible {
+                        case one
+
+                        var description: String {
+                            "bye :("
+                        }
+                    }
+                    """
+                expectedResult = """
+                    enum Value: CustomStringConvertible {
+                        case one
+
+                        var description: String {
+                            "bye :("
+                        }
+                    }
+
+                    public struct ValueWrapper: CustomStringConvertible, Equatable, Hashable, Sendable {
+                        typealias WrappedValue = Value
+
+                        let wrappedValue: WrappedValue
+
+                        private init(wrappedValue: WrappedValue) {
+                            self.wrappedValue = wrappedValue
+                        }
+
+                        public static let one = Self.init(wrappedValue: .one)
+
+                        // MARK: CustomStringConvertible
+
+                        public var description: String {
+                            wrappedValue.description
+                        }
+                    }
+                    """
+            case .decodable:
+                input = """
+                    @PublicWrapper
+                    enum Value: Decodable {
+                        case one
+                    }
+                    """
+                expectedResult = """
+                    enum Value: Decodable {
+                        case one
+                    }
+
+                    public struct ValueWrapper: Decodable, Equatable, Hashable, Sendable {
+                        typealias WrappedValue = Value
+
+                        let wrappedValue: WrappedValue
+
+                        private init(wrappedValue: WrappedValue) {
+                            self.wrappedValue = wrappedValue
+                        }
+
+                        public static let one = Self.init(wrappedValue: .one)
+
+                        // MARK: Decodable
+
+                        public init(from decoder: any Decoder) throws {
+                            self.wrappedValue = try WrappedValue(from: decoder)
+                        }
+                    }
+                    """
+            case .encodable:
+                input = """
+                    @PublicWrapper
+                    enum Value: Encodable {
+                        case one
+                    }
+                    """
+                expectedResult = """
+                    enum Value: Encodable {
+                        case one
+                    }
+
+                    public struct ValueWrapper: Encodable, Equatable, Hashable, Sendable {
+                        typealias WrappedValue = Value
+
+                        let wrappedValue: WrappedValue
+
+                        private init(wrappedValue: WrappedValue) {
+                            self.wrappedValue = wrappedValue
+                        }
+
+                        public static let one = Self.init(wrappedValue: .one)
+
+                        // MARK: Encodable
+
+                        public func encode(to encoder: any Encoder) throws {
+                            try wrappedValue.encode(to: encoder)
+                        }
+                    }
+                    """
+            case .equatable:
+                input = """
+                    @PublicWrapper
+                    enum Value: Equatable {
+                        case one
+                    }
+                    """
+                expectedResult = """
+                    enum Value: Equatable {
+                        case one
+                    }
+
+                    public struct ValueWrapper: Equatable, Hashable, Sendable {
+                        typealias WrappedValue = Value
+
+                        let wrappedValue: WrappedValue
+
+                        private init(wrappedValue: WrappedValue) {
+                            self.wrappedValue = wrappedValue
+                        }
+
+                        public static let one = Self.init(wrappedValue: .one)
+                    }
+                    """
+            case .hashable:
+                input = """
+                    @PublicWrapper
+                    enum Value: Hashable {
+                        case one
+                    }
+                    """
+                expectedResult = """
+                    enum Value: Hashable {
+                        case one
+                    }
+
+                    public struct ValueWrapper: Hashable, Equatable, Sendable {
+                        typealias WrappedValue = Value
+
+                        let wrappedValue: WrappedValue
+
+                        private init(wrappedValue: WrappedValue) {
+                            self.wrappedValue = wrappedValue
+                        }
+
+                        public static let one = Self.init(wrappedValue: .one)
+                    }
+                    """
+            case let .identifiable(type):
+                input = """
+                    @PublicWrapper
+                    enum Value: Identifiable {
+                        case one
+
+                        var id: \(type.rawValue) {
+                            "\\(self)"
+                        }
+                    }
+                    """
+                expectedResult = """
+                    enum Value: Identifiable {
+                        case one
+
+                        var id: \(type.rawValue) {
+                            "\\(self)"
+                        }
+                    }
+
+                    public struct ValueWrapper: Identifiable, Equatable, Hashable, Sendable {
+                        typealias WrappedValue = Value
+
+                        let wrappedValue: WrappedValue
+
+                        private init(wrappedValue: WrappedValue) {
+                            self.wrappedValue = wrappedValue
+                        }
+
+                        public static let one = Self.init(wrappedValue: .one)
+
+                        // MARK: Identifiable
+
+                        public var id: \(type.rawValue) {
+                            wrappedValue.id
+                        }
+                    }
+                    """
+            case .sendable:
+                input = """
+                    @PublicWrapper
+                    enum Value: Sendable {
+                        case one
+                    }
+                    """
+                expectedResult = """
+                    enum Value: Sendable {
+                        case one
+                    }
+
+                    public struct ValueWrapper: Sendable, Equatable, Hashable {
+                        typealias WrappedValue = Value
+
+                        let wrappedValue: WrappedValue
+
+                        private init(wrappedValue: WrappedValue) {
+                            self.wrappedValue = wrappedValue
+                        }
+
+                        public static let one = Self.init(wrappedValue: .one)
+                    }
+                    """
             }
-            """
-        assertMacroExpansion(
-            input,
-            expandedSource: expectedResult,
-            macroSpecs: testMacros,
-            failureHandler: Issue.record(failure:)
-        )
+
+            return (input, expectedResult)
+        }
     }
-}
+
+#endif
